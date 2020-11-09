75e8098277d165f0148f6b6cfc05f7e6cf4efa00
michalpycia <michalpycia@gmail.com>
2020-11-09 21:09:01 +0100

    Static files

--- oddajrzeczy/views.py
+++ oddajrzeczy/views.py
@@ -1,3 +1,10 @@
+from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'index.html')
